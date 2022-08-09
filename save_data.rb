8f6b1c1736d397658f346a320ce78b5f16265421
ilham bouaik <ibouaik@gmail.com>
2022-08-09 11:10:46 +0100

    store data in files

--- save_data.rb
+++ save_data.rb
@@ -1,6 +1,5 @@
 require_relative 'input'
 require 'json'
-
 class SaveData < Input
   def initialize
     super()
@@ -11,11 +10,25 @@ class SaveData < Input
     if File.empty?('books.json')
       puts 'List is empty'
     else
-        books = JSON.parse(File.read('books.json'))
-        books.each do |book|
-            @books.push(Book.new['Title'], book['Author'])
-        end
+      books = JSON.parse(File.read('books.json'))
+      books.each do |book|
+        @books.push(Book.new['Title'], book['Author'])
+      end
     end
 
     File.write('books.json', JSON.generate(books))
+
+    if File.empty?('person.json')
+      puts 'List is empty'
+    else
+      persons = JSON.parse(File.read('person.json'))
+      persons.each do |person|
+        if persons['Class'] == 'Student'
+          @persons.push(Student.new(person['Age'], person['Name'], person['parent_permission']))
+        else
+          persons.push(Teacher.new(person['specialization'], person['Age'], person['Name']))
+        end
+      end
+    end
+  end
 end
